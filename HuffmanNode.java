e0e18fdff2cfd6c66935a56d65c1934a6030ec5b
Ethan Bai <111791598+ebai2022@users.noreply.github.com>
2023-01-01 20:57:46 -0800

    Add files via upload

--- HuffmanNode.java
+++ HuffmanNode.java
@@ -1,25 +1,36 @@
 // Ethan Bai
 // CSE 143 AR with Adithi Srikanth Raghavan
 // Homework 8
-// HuffmanNode
+// HuffmanNode defines the HuffmanNode used within HuffmanTree. A HuffmanNode
+// has a right and left branch and can store the frequency and character values
 public class HuffmanNode implements Comparable<HuffmanNode>{
    public HuffmanNode left;
    public HuffmanNode right;
    public int frequency;
    public int character;   
    
+   // parameters : takes an int frequency and an int character that define the value of the 
+   // frequency and the character
+   // post : creates a HuffmanNode for use in the HuffmanTree
    public HuffmanNode(int frequency, int character){
       this(frequency, character, null, null);
    }
    
+   // parameters : takes an int frequency that defines the frequency the character appears, an
+   // int character that defines the value of the character, a HuffmanNode left that serves as 
+   // the left branch, and a HuffmanNode right that serves as the right branch
+   // post : creates a HuffmanNode for use in the HuffmanTree
    public HuffmanNode(int frequency, int character, HuffmanNode left, HuffmanNode right){
       this.frequency = frequency;
       this.character = character;
       this.right = right;
       this.left = left;
-      //System.out.println(character + " " + frequency);
    }
    
+   // parameters : takes a HuffmanNode other to compare to the current HuffmanNode
+   // post : returns a negative integer if the frequency of the current HuffmanNode is smaller.
+   // Returns zero if the two frequencies are equal. Returns a positive integer if the other
+   // frequency is greater than the current frequency
    public int compareTo(HuffmanNode other){
       return (this.frequency - other.frequency);
    }
--- HuffmanTree.java
+++ HuffmanTree.java
@@ -1,14 +1,23 @@
 // Ethan Bai
 // CSE 143 AR with Adithi Srikanth Raghavan
 // Homework 8
+// HuffmanTree is able to take either an array of frequencies where each index relates to a
+// character or a scanner input as its file and create a decision tree based off the 
+// information given. HuffmanTree is also able to write text files into their respective code
+// values and write (decipher) short (binary) files back into text form. Generally, 
+// character and frequency for parts of the HuffmanTree not relating to an explicit character 
+// are defaulted to a value of -1 for their respective character and frequency values depending
+// on the information given
 import java.util.*;
 import java.io.*;
 public class HuffmanTree{
    private HuffmanNode overallRoot;
-   private Queue<HuffmanNode> nodeStorage;
    
+   // parameters : takes an int[] count that serves as an array of characters that contains the
+   // frequency of each respective character at each index
+   // post : initializes the HuffmanTree with its respective values (sorted in Huffman order)
    public HuffmanTree(int[] count){
-      nodeStorage = new PriorityQueue<>();
+      Queue<HuffmanNode> nodeStorage = new PriorityQueue<>();
       for (int i = 0; i < count.length; i++){
          if (count[i] > 0){
             HuffmanNode letter = new HuffmanNode(count[i], i);
@@ -16,55 +25,55 @@ public class HuffmanTree{
          }
       }
       nodeStorage.add(new HuffmanNode(1, count.length));
-      /*while (!nodeStorage.isEmpty()){
-         System.out.println(nodeStorage.remove().frequency);
-      }*/
       while (nodeStorage.size() > 1){
          HuffmanNode first = nodeStorage.remove();
          HuffmanNode second = nodeStorage.remove();
          nodeStorage.add(new HuffmanNode(first.frequency + second.frequency, -1, first, second));
       }
       overallRoot = nodeStorage.remove();
-      nodeStorage.add(overallRoot);
    }
    
-   // parameters : 
+   // parameters : takes a Scanner input that serves as the file for the HuffmanTree
+   // post : initializes the HuffmanTree with its respective values (sorted in Huffman order) 
    public HuffmanTree(Scanner input){
-      int n = Integer.parseInt(input.nextLine());
-      String code = input.nextLine();
       while (input.hasNextLine()){
+         int n = Integer.parseInt(input.nextLine());
+         String code = input.nextLine();
          overallRoot = treeConstructor(overallRoot, n, code);
-         n = Integer.parseInt(input.nextLine());
-         code = input.nextLine();
       }
    }
    
-   // pre :
+   // helper method for HuffmanTree(Scanner input). takes a Scanner input that serves as the 
+   // file for the HuffmanTree, an int n for the character value, and a String code which 
+   // indicates left or right (based off the given '0's and/or '1's pattern). Constructs the  
+   // HuffmanTree with its respective values. Does not check for errors
    private HuffmanNode treeConstructor(HuffmanNode root, int n, String code){
-      if (code.length() == 0){
+      if (code.isEmpty()){
          return new HuffmanNode(-1, n);
       } 
       if (root == null){
          root = new HuffmanNode(-1, -1);
       }
       if (code.charAt(0) == '0'){
-         root.left = treeConstructor(root.left, n, code.substring(1, code.length()));
+         root.left = treeConstructor(root.left, n, code.substring(1));
       } else{
-         root.right = treeConstructor(root.right, n, code.substring(1, code.length()));
+         root.right = treeConstructor(root.right, n, code.substring(1));
       }
       return root;
    }
    
    // parameters : takes a PrintStream output to write into
    // pre : the given PrintStream is able to be written into
-   // post : writes the data from the current AOFIJOIJRWF tree into
-   // the given "file" (PrintStream output)
+   // post : writes the data from the current HuffmanTree tree into
+   // the given "file" (PrintStream output) in standard format
    public void write(PrintStream output){
       writer(overallRoot, output, "");
    }
    
-   // helper method for write. Does not check for errors. Writes the data
-   // given from theDFJAODJFOIADJOFJAE tree into the given "file" (PrintStream output)
+   // helper method for write. Does not check for errors. Takes a PrintStream output to write
+   // into, a HuffmanNode root to use to find each character, and a String tracker to track 
+   // the left and right motions (using '0' for left or '1' for right). Writes the data
+   // given from the HuffmanTree into the given "file" (PrintStream output) standard format
    private void writer(HuffmanNode root, PrintStream output, String tracker){
       if (root != null){
          if (root.left == null && root.right == null){
@@ -77,26 +86,25 @@ public class HuffmanTree{
       }
    }
    
-   // yee
+   // parameters : takes a BitInputStream input that is used for decoding the HuffmanTree, a
+   // PrintStream output to write into, and an int eof that indicates the eof value
+   // post : writes the data from the given input into the given "file" (PrintStream output)
    public void decode(BitInputStream input, PrintStream output, int eof){
-      int nextBit = input.readBit();
-      while (nextBit != -1){
-         nextBit = decoder(input, output, eof, overallRoot, nextBit);
-      }
-   }
-   
-   // blah blah IM CALLING readBit ONE EXTRA TIME PER ROTATION - HOW TO FIX? Stop 1 before?
-   private int decoder(BitInputStream input, PrintStream output, int eof, HuffmanNode root, int bit){
-      if (root != null && bit != -1 && root.character != eof){
-         if (root.left == null && root.right == null){
-            output.write(root.character);
-            return bit;
-         } else if (bit == 0){
-            bit = decoder(input, output, eof, root.left, input.readBit());            
-         } else{
-            bit = decoder(input, output, eof, root.right, input.readBit()); 
+      int character = 0;
+      while (character != eof){
+         HuffmanNode root = overallRoot;
+         while (root.left != null || root.right != null){
+            int bit = input.readBit();
+            if (bit == 0){
+               root = root.left;
+            } else{
+               root = root.right;
+            }
+         }
+         character = root.character;
+         if (character != eof){
+            output.write(character);
          }
       }
-      return bit;
-   }
+   }   
 }
